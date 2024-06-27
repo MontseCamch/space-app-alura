7f7bac5fc82978c09445b525612a41e4505391a7
MontseCamch <157327556+MontseCamch@users.noreply.github.com>
2024-06-26 23:20:51 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://MontseCamch.github.io/space-app-alura',
+  plugins: [react()]
 })
